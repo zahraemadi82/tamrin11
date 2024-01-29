079a19b36b9db9cdfd39898d6d4bc180714dbc6e
User <User@DESKTOP-1364VBG>
2024-01-29 18:13:20 +0330

    Add project files.

---
+++ tamrin1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace project 
+{
+    public class Teacher
+    {
+        private string id;
+        private string name;
+        private string lastname;
+        private int hour;
+        private int payoerhour;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        public int Hour
+        {
+            get { return hour; }
+            set { hour = value; }
+        }
+        public int Payperhour
+        {
+            get { return payoerhour; }
+            set
+            {
+                if (value > 0)
+                {
+                    payoerhour = value;
+
+                }
+            }
+        }
+        public Teacher(string teacherId, string teacherName, string teacherLastname, int teacherHour, int teacherPayperhour)
+        {
+            id = teacherId;
+            name = teacherName;
+            lastname = teacherLastname;
+            hour = teacherHour;
+            payoerhour = teacherPayperhour;
+        }
+
+
+        public double payment()
+        {
+            return hour * payoerhour;
+        }
+    }
+
+
+
+    class test
+    {
+        static void Main(string[] args)
+        {
+            Teacher th = new Teacher("123", "Ali", "Ahmadi", 40, 4000);
+            double pay = th.payment();
+            Console.WriteLine(pay);
+
+        }
+    }
+}
